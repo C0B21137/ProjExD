55326808f9ac2cc11465cd4123e1f96eff7b7539
c0b21137 <c0b211376a@edu.teu.ac.jp>
2022-09-27 15:37:55 +0900

    demo

--- lec01/quiz.py
+++ lec01/quiz.py
@@ -1,16 +1,16 @@
 import random
 import datetime
-rnd=random.randint(0,3)
+rnd=random.randint(0,2)
 que=["サザエの旦那の名前は？","カツオの妹の名前は?","タラオはカツオから見てどんな関係？"]
-ans=["ますお","マスオ","わかめ","ワカメ","おい","甥","甥っ子","おいっこ"]
+ans=["ますお" "マスオ","わかめ" "ワカメ","おい" "甥" "甥っ子" "おいっこ"]
 st=datetime.datetime.now()
 def Syutudai():
     q=print(f"問題:{que[rnd]}")
-    ua=input(f"解答:")
+    ua=input("解答:")
     return ua
 
 def Kaito(ua):
-    if ua in ans:
+    if ua in ans[rnd]:
         print("正解!")
     else:
         print("カスめ")
