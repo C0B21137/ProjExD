6a8f39143cfd2cfb531ae7dd4b4fae1e9c0570fd
c0b21137 <c0b211376a@edu.teu.ac.jp>
2022-10-04 15:55:08 +0900

    四則演算

--- lec02/calc.py
+++ lec02/calc.py
@@ -1,39 +1,47 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def button_click(event):
+def button_click(event):#ボタンを押したときの関数
     button=event.widget
     number=(button["text"])
     #tkm.showinfo({number},f"{number}のボタンが押されました。")
     entry.insert(tk.END,number)
 
-def click_eq(event):
+def click_eq(event):#＝を押したときの関数
     eq=entry.get()
     res=eval(eq)
     entry.delete(0,tk.END)
     entry.insert(tk.END,res)
 
-root=tk.Tk()
+root=tk.Tk()#ウィンドウを作る
 root.title("calc")
-root.geometry("300x600")
+root.geometry("1000x550")
 
-entry=tk.Entry(root,justify="right",width=10,font=("Times New Roma",40))
+entry=tk.Entry(root,justify="right",width=10,font=("Times New Roma",40))#表示バー
 entry.grid(row=0,column=0,columnspan=3)
 
-c=0
+c=0 #数字のボタン
 for i in range(10):
-    button=tk.Button(root,text=f"{9-i}",font=("",30),width=4,height=2)
-    button.bind("<1>",button_click)
-    button.grid(row=c+1,column=i%3)
+    button_num=tk.Button(root,text=f"{9-i}",font=("",30),width=4,height=2)
+    button_num.bind("<1>",button_click)
+    button_num.grid(row=c+1,column=i%3)
     if i%3==2:
         c+=1
 
+#　+ボタン
 button_p=tk.Button(root,text="+",font=("",30),width=4,height=2)
 button_p.bind("<1>",button_click)
 button_p.grid(row=4,column=1)
-
+#　＝ボタン
 button_eq=tk.Button(root,text="=",font=("",30),width=4,height=2)
 button_eq.bind("<1>",click_eq)
 button_eq.grid(row=4,column=2)
 
+s=["-","*","/"]
+a=0
+for i in s:
+    button_s=tk.Button(root,text=i,font=("",30),width=4,height=2)
+    button_s.bind("<1>",button_click)
+    button_s.grid(row=0,column=4+a)
+    a+=1
 root.mainloop()
