064e2b0af155bf872c15033e039fff807d3998ba
c0b21137 <c0b211376a@edu.teu.ac.jp>
2022-10-04 14:22:49 +0900

    2

--- lec02/calc.py
+++ lec02/calc.py
@@ -1,23 +1,14 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
-def button_click():
-    tkm.showwarning("警告","ボタン押したらダメ言うたやろ")
-print("hello world")
+
 root=tk.Tk()
-root.title("おためしか")
+root.title("calc")
 root.geometry("300x500")
 
-label=tk.Label(root,
-text="ラベルを書いてみた件",
-font=("Times New Roman",20))
-label.pack()
-
-button=tk.Button(root,text="押すな",font=("Times New Roman",10),width=4,height=2,command=button_click())
-button.pack()
-
-entry=tk.Entry(width=30)
-entry.insert(tk.END,"fugapiyo")
-entry.pack()
-
-
+c=0
+for i in range(10):
+    button=tk.Button(root,text=9-i,font=("Times New Roman",30),width=4,height=2)
+    button.grid(row=c,column=i%3)
+    if i%3==2:
+        c+=1
 root.mainloop()
