7c69e21e4db0ed712ed1df1842b35cd51f8cc091
c0b21137 <c0b211376a@edu.teu.ac.jp>
2022-10-04 14:39:55 +0900

    3

--- lec02/calc.py
+++ lec02/calc.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
+def button_click(event):
+    button=event.widget
+    number=int(button["text"])
+    tkm.showinfo({number},f"{number}のボタンが押されました。")
 
 root=tk.Tk()
 root.title("calc")
@@ -8,7 +12,10 @@ root.geometry("300x500")
 c=0
 for i in range(10):
     button=tk.Button(root,text=9-i,font=("Times New Roman",30),width=4,height=2)
+    button.bind=("<1>",button_click)
     button.grid(row=c,column=i%3)
     if i%3==2:
         c+=1
+
+
 root.mainloop()
