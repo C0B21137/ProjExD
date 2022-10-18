2970cd319c467f998c70dece81fb066a14894c82
c0b21137 <c0b211376a@edu.teu.ac.jp>
2022-10-18 14:44:50 +0900

    2

--- lec03/maze.py
+++ lec03/maze.py
@@ -4,4 +4,8 @@ import tkinter.messagebox as tkm
 if __name__=="__main__":
     root=tk.Tk()
     root.title("迷えるこうかとん") #1
+
+    can=tk.Canvas(root,width=1500,height=900,bg="black") #2
+    can.pack()
+    
     root.mainloop()
