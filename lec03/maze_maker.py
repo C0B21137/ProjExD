f3a1969171c9e7719267bc186791777695cfec41
c0b21137 <c0b211376a@edu.teu.ac.jp>
2022-10-18 14:18:17 +0900

    3

--- lec03/maze_maker.py
+++ lec03/maze_maker.py
@@ -38,4 +38,7 @@ if __name__=="__main__":
     root.title("迷えるこうかとん")
     can=tk.Canvas(root,width=1500,height=900,bg="black")
     can.pack()
+    tori=tk.PhotoImage(file="fig/5.png")
+    cx,cy=300,400
+    can.create_image(cx,cy,image=tori,tag="tori")
     root.mainloop()
