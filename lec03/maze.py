24771618185188ed0525e37b15b6ad1af3593847
c0b21137 <c0b211376a@edu.teu.ac.jp>
2022-10-18 16:21:54 +0900

    goal

--- lec03/maze.py
+++ lec03/maze.py
@@ -12,7 +12,7 @@ def key_up(event): #6
 
 def main_proc(): #7 #11
     global mx,my
-    global cx,cy
+    global cx,cy,a
     if key=="Up":
         my-=1
     if key=="Down":
@@ -21,13 +21,29 @@ def main_proc(): #7 #11
         mx-=1
     if key=="Right":
         mx+=1
-    cx,cy=mx*100,my*100
-    can.coords("tori",cx,cy)
-    root.after(100,main_proc)
+    if maze_list[my][mx]==0:
+        cx,cy=mx*100+50,my*100+50
+    else:
+        if key=="Up":
+            my+=1
+        if key=="Down":
+            my-=1
+        if key=="Left":
+            mx+=1
+        if key=="Right":
+            mx-=1
+    can.coords("tori",cx,cy)    
+    if mx==13 and my==7:  
+        tkm.showinfo("goal","goal")  
+        mx==1
+        my==1
+    else:    
+        root.after(100,main_proc)
 
 if __name__=="__main__":
     root=tk.Tk()
     root.title("迷えるこうかとん") #1
+    a=0
 
     can=tk.Canvas(root,width=1500,height=900,bg="black") #2
     can.pack()
@@ -36,8 +52,8 @@ if __name__=="__main__":
     mm.show_maze(can,maze_list) #10
 
     tori=tk.PhotoImage(file="fig/5.png") #3
-    mx,my=1.5,1.5#11
-    cx,cy=mx*100,my*100
+    mx,my=1,1#11
+    cx,cy=mx*100+50,my*100+50
     can.create_image(cx,cy,image=tori,tag="tori")
 
     key="" #4
