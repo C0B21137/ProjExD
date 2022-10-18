66edcf7b997ed1b24b390479690159ef15a70552
c0b21137 <c0b211376a@edu.teu.ac.jp>
2022-10-18 14:45:12 +0900

    3

--- lec03/maze.py
+++ lec03/maze.py
@@ -7,5 +7,8 @@ if __name__=="__main__":
 
     can=tk.Canvas(root,width=1500,height=900,bg="black") #2
     can.pack()
-    
+
+    tori=tk.PhotoImage(file="fig/5.png") #3
+    cx,cy=300,400
+    can.create_image(cx,cy,image=tori,tag="tori")
     root.mainloop()
