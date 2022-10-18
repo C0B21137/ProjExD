fcbe71b6021aa27feabe73ab5648f78c23e8f5d2
c0b21137 <c0b211376a@edu.teu.ac.jp>
2022-10-18 14:48:18 +0900

    miss

--- lec03/maze_maker.py
+++ lec03/maze_maker.py
@@ -1,6 +1,4 @@
 import random
-import tkinter as tk
-import tkinter.messagebox as tkm
 
 def make_maze(yoko, tate):
     XP = [ 0, 1, 0, -1]
@@ -33,45 +31,3 @@ def show_maze(canvas, maze_lst):
             canvas.create_rectangle(x*100, y*100, x*100+100, y*100+100, 
                                     fill=color[maze_lst[y][x]])
 
-def key_down(event): #5
-    global key
-    key=event.keysym
-
-def key_up(event): #6
-    global key
-    key=""
-
-def main_proc(): #7
-    global cx,cy
-    if key=="Up":
-        cy-=20
-    if key=="Down":
-        cy+=20
-    if key=="Left":
-        cx-=20
-    if key=="Right":
-        cx+=20
-    can.coords("tori",cx,cy)
-    root.after(100,main_proc)
-   
-if __name__=="__main__":
-    root=tk.Tk()
-    root.title("迷えるこうかとん") #1
-
-    can=tk.Canvas(root,width=1500,height=900,bg="black") #2
-    can.pack()
-
-    tori=tk.PhotoImage(file="fig/5.png") #3
-    cx,cy=300,400
-    can.create_image(cx,cy,image=tori,tag="tori")
-
-    key="" #4
-
-    root.bind("<KeyPress>",key_down) #5
-
-    root.bind("<KeyRelease>",key_up) #6
-
-    can.coords("tori",cx,cy) #7
-    main_proc()
-
-    root.mainloop()
