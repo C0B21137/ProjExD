b73d6270f63405965d4f42c75fd5bdc58a2fe53b
c0b21137 <c0b211376a@edu.teu.ac.jp>
2022-10-18 15:20:14 +0900

    11

--- lec03/maze.py
+++ lec03/maze.py
@@ -10,16 +10,18 @@ def key_up(event): #6
     global key
     key=""
 
-def main_proc(): #7
+def main_proc(): #7 #11
+    global mx,my
     global cx,cy
     if key=="Up":
-        cy-=20
+        my-=1
     if key=="Down":
-        cy+=20
+        my+=1
     if key=="Left":
-        cx-=20
+        mx-=1
     if key=="Right":
-        cx+=20
+        mx+=1
+    cx,cy=mx*100,my*100
     can.coords("tori",cx,cy)
     root.after(100,main_proc)
 
@@ -30,8 +32,12 @@ if __name__=="__main__":
     can=tk.Canvas(root,width=1500,height=900,bg="black") #2
     can.pack()
 
+    maze_list=mm.make_maze(15,9) #9
+    mm.show_maze(can,maze_list) #10
+
     tori=tk.PhotoImage(file="fig/5.png") #3
-    cx,cy=300,400
+    mx,my=1.5,1.5#11
+    cx,cy=mx*100,my*100
     can.create_image(cx,cy,image=tori,tag="tori")
 
     key="" #4
@@ -43,6 +49,4 @@ if __name__=="__main__":
     can.coords("tori",cx,cy) #7
     main_proc()
 
-    maze_list=mm.make_maze(15,9) #9
-    mm.show_maze(can,maze_list) #10
     root.mainloop()
